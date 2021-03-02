e816c32c25c68b9cefb9c7feaf0dcdaae71d2d31
Tharshan Kumar R G <61736284+tharshan18@users.noreply.github.com>
2021-03-02 20:22:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Expert &mdash; Onepage Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Muli:300,400,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+      
+      <div class="container-fluid">
+        <div class="d-flex align-items-center">
+          <div class="site-logo"><a href="index.html" class="text-uppercase">T1 E-PASS</a></div>
+          <div class="ml-auto">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-xl-block">
+              <li class="cta"><a href="Epassuser/Otp/index.php" class="nav-link"><span class="border bg-danger rounded text-white border-danger">Login</span></a></li>
+
+              </ul>
+            </nav>
+            <a href="#" class="d-inline-block d-xl-none site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a>
+          </div>
+        </div>
+      </div>
+      
+    </header>
+
+    <div class="intro-section custom-owl-carousel" id="home-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-5 mr-auto" data-aos="fade-up">
+
+            <div class="owl-carousel slide-one-item-alt-text">
+              <div class="slide-text">
+                <h1>MASK UP</h1>
+                <p class="mb-5">Masks are most likely to reduce the spread of COVID-19 when they are widely used by people in public settings.</p>
+                <p class="text">#STAY_HOME #STAY_SAFE</a></p>
+              </div>
+              <div class="slide-text">
+                <h1>SOCIAL DISTANCNIG</h1>
+                <p class="mb-5">Maintaining socail distancing atleast of 2 feet at public places is very important.Social distancing is especially important for people who are at higher risk for severe illness from COVID-19.</p>
+                <p class="text">#STAY_HOME #STAY_SAFE</a></p>
+              </div>
+              <div class="slide-text">
+                <h1>AVOID TOUCHING MEN</h1>
+                <p class="mb-5">
+                  M - Mouth<br>
+                  E - Eyes<br>
+                  N - Nose<br>
+                </p>
+                <p class="text">#STAY_HOME #STAY_SAFE</a></p>
+              </div>
+              <div class="slide-text">
+                <h1>INSTEAD, FOLLOW<br>WOMEN</h1>
+                <p class="mb-5">
+                  W - Wash your hands<br>
+                  O - Obey social distancing<br>
+                  M - Mask up<br>
+                  E - Exercise and Eat well<br>
+                  N - No unnecessary crowding<br>
+                </p>
+                <p class="text">#STAY_HOME #STAY_SAFE</a></p>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-6 ml-auto"  data-aos="fade-up" data-aos-delay="100">
+                        
+            <div class="owl-carousel slide-one-item-alt">
+              <img src="images/mask up.jpg" alt="Image" class="img-fluid">
+              <img src="images/soci.jpg" alt="Image" class="img-fluid">
+              <img src="images/dont touch men.jpg" alt="Image" class="img-fluid">
+              <img src="images/follow women.jpg" alt="Image" class="img-fluid">
+            </div>
+             
+            <div class="owl-custom-direction">
+              <a href="#" class="custom-prev"><span class="icon-keyboard_arrow_left"></span></a>
+              <a href="#" class="custom-next"><span class="icon-keyboard_arrow_right"></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div>
+          <p>Done By:<br><br><b>TEAM 38</b><br>
+          Nandha Kumar B<br>Sri Jayanth P<br>Tharshan Kumar R G<br>Rohit T</p>
+          </div> -->
+          <footer class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+      
+      <div class="container-fluid">
+        <div class="d-flex align-items-center">
+          <div class="ml-auto">
+            <nav class="site-navigation position-relative text-right">
+              <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-xl-block">
+              <li class="cta"><a href="#contact-section" class="nav-link"><span class="rounded text-black text-align:left">Done By:<br><br><b>TEAM 38</b><br>
+          Nandha Kumar N<br>Sri Jayanth P<br>Tharshan Kumar R G<br>Rohit T<br></span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="d-inline-block d-xl-none site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a>
+          </div>
+        </div>
+      </div>
+      </footer>
+    <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/main.js"></script>
+    
+  
+  </body>
+</html>
